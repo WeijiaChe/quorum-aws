a49acc63f38ce939521190f1d41a8fcac60f97de
WeijiaChe <Weijia.Che@gmail.com>
2018-03-22 21:45:03 -0700

    Add additional params to quorum start script

--- terraform/scripts/install/start-quorum.sh
+++ terraform/scripts/install/start-quorum.sh
@@ -9,4 +9,6 @@ raft_port=$((50400 + $gid))
 
 echo "starting geth ${gid}"
 
-sudo docker run -d -p $p2p_port:$p2p_port -p $rpc_port:$rpc_port -p $raft_port:$raft_port -v /home/ubuntu/datadir:/datadir -v /home/ubuntu/password:/password -e PRIVATE_CONFIG='/datadir/constellation.toml' quorum --datadir /datadir --port $p2p_port --rpcport $rpc_port --raftport $raft_port --verbosity 3 --nodiscover --rpc --rpccorsdomain "'*'" --rpcaddr '0.0.0.0' --raft --unlock 0 --password /password
+ARGS="--rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints"
+
+sudo docker run -d -p $p2p_port:$p2p_port -p $rpc_port:$rpc_port -p $raft_port:$raft_port -v /home/ubuntu/datadir:/datadir -v /home/ubuntu/password:/password -e PRIVATE_CONFIG='/datadir/constellation.toml' quorum --datadir /datadir $ARGS --port $p2p_port --rpcport $rpc_port --raftport $raft_port --verbosity 3 --nodiscover --rpc --rpccorsdomain "'*'" --rpcaddr '0.0.0.0' --raft --unlock 0 --password /password
